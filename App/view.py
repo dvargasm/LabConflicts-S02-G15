e863bb0b0492903f1e35e5a690770f781b178903
Daniel Vargas <d.vargasm@uniandes.edu.co>
2021-08-25 13:27:02 -0500

    se agrego la rama est-1

--- App/view.py
+++ App/view.py
@@ -34,7 +34,7 @@ assert cf
 La vista se encarga de la interacción con el usuario
 Presenta el menu de opciones  y  por cada seleccion
 se hace la solicitud al controlador para ejecutar la
-operación solicitada
+operación solicitada.
 """
 
 
